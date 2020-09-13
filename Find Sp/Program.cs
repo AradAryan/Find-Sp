a8163f16fd3106e672086a02218f6ff7cd21eadf
Arad <AradAryanBusiness@gmail.com>
2020-09-13 13:31:52 +0430

    Add project files.

---
+++ Find Sp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Find_Sp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Directory.GetFiles(@"C:\Users\javad\Desktop\Afzal DB Context", "*.Context.cs");
+            List<string> names = new List<string>();
+            foreach (var item in list)
+            {
+                var lines = File.ReadAllLines(item);
+                foreach (var name in lines.Where(p => p.Contains("SP_Afzal_GetTrRecords")))
+                {
+                    names.Add(name);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
